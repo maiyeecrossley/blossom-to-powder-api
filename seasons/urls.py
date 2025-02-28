9d4af490fecbd48d220d041b515606ffd1049236
maiyeecrossley <maiyee.crossley@gmail.com>
2025-02-28 12:41:15 +0000

    updated urls in seasons app

--- seasons/urls.py
+++ seasons/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SeasonDateLocationsView
+from .views import SeasonDateLocationsView, SeasonalLocationsView
 
 urlpatterns = [
     path('locations/', SeasonDateLocationsView.as_view()),
+    path('<int:season_id>/locations/', SeasonalLocationsView.as_view())
 ]
